671354b38bd7c345230188a375f79b64b9dd5234
Florent Huck <flovntp@gmail.com>
2025-05-16 13:56:22 +0200

    improve OrganizationTask

--- src/Core/Tasks/OrganizationTask.php
+++ src/Core/Tasks/OrganizationTask.php
@@ -21,6 +21,6 @@ class OrganizationTask extends TaskBase
     public function list($filter_id = null, $filter_owner_id = null, $filter_name = null, $filter_label = null, $filter_vendor = null, $filter_capabilities = null, $filter_status = null, $filter_updated_at = null, $page_size = null, $page_before = null, $page_after = null, $sort = null, string $contentType = '') {
         var_dump($this->api->listOrgsRequest($filter_id, $filter_owner_id, $filter_name, $filter_label, $filter_vendor, $filter_capabilities, $filter_status, $filter_updated_at, $page_size, $page_before, $page_after, $sort, $contentType));
         
-        return $this->api->listOrgs($filter_id, $filter_owner_id, $filter_name, $filter_label, $filter_vendor, $filter_capabilities, $filter_status, $filter_updated_at, $page_size, $page_before, $page_after, $sort, $contentType);
+        return $this->api->listOrgs($filter_id, $filter_owner_id, $filter_name, $filter_label, $filter_vendor, $filter_capabilities, $filter_status, $filter_updated_at, 1000, $page_before, $page_after, $sort, $contentType);
     }
 }
