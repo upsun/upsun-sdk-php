678679857ef4be5e42c46b3cc1c6783e19dfe02b
Florent Huck <flovntp@gmail.com>
2025-05-16 12:39:18 +0200

    improve OrganizationTask

--- src/Core/Tasks/TaskBase.php
+++ src/Core/Tasks/TaskBase.php
@@ -3,9 +3,16 @@
 namespace Upsun\Core\Tasks;
 
 //use GuzzleHttp\Client;
+use OpenAPI\Client\apisgen\OrganizationsApi;
 use Upsun\UpsunClient;
 
 abstract class TaskBase
 {
-    public function __construct(public readonly UpsunClient $client) { }
+    public function __construct(
+        public readonly UpsunClient $client,
+        public $api
+    )
+    {
+        $this->api = new OrganizationsApi($this->client->apiClient, $this->client->apiConfig);
+    }
 }
