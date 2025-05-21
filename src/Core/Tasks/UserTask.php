c4df7171a8b104e509f8646bf6ab353b0bdf43ba
Florent Huck <flovntp@gmail.com>
2025-05-21 10:09:25 +0200

    add UserTask.getUserFullName

--- src/Core/Tasks/UserTask.php
+++ src/Core/Tasks/UserTask.php
@@ -8,22 +8,31 @@ use Upsun\UpsunClient;
 class UserTask extends TaskBase
 {
 
-    public readonly UsersApi $api;
-    
-    public function __construct(
-        public readonly UpsunClient $client,
-    )
-    {
-        $this->api = new UsersApi($this->client->apiClient, $this->client->apiConfig);
-    }
-    
-    public function me() {
-        $this->refreshToken();
-        return $this->api->getCurrentUser();
-    }
-    
-    public function getUser(string $id) {
-        $this->refreshToken();
-        return $this->api->getUser($id);
-    }
+  public readonly UsersApi $api;
+
+  public function __construct(
+    public readonly UpsunClient $client,
+  )
+  {
+    $this->api = new UsersApi($this->client->apiClient, $this->client->apiConfig);
+  }
+
+  public function me()
+  {
+    $this->refreshToken();
+    return $this->api->getCurrentUser();
+  }
+
+  public function getUser(string $id)
+  {
+    $this->refreshToken();
+    return $this->api->getUser($id);
+  }
+
+  public function getUserFullName(string $id)
+  {
+    $this->refreshToken();
+    $user = $this->api->getUser($id);
+    return trim($user->getFirstName(). ' ' . $user->getLastName());  
+  }
 }
