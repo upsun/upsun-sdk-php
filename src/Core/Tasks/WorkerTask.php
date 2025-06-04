95095864cef2293f2a6536e52a4b0cb96a6be6c4
Florent Huck <flovntp@gmail.com>
2025-06-04 17:01:16 +0200

    WorkerTask.list

--- src/Core/Tasks/WorkerTask.php
+++ src/Core/Tasks/WorkerTask.php
@@ -2,7 +2,37 @@
 
 namespace Upsun\Core\Tasks;
 
+use OpenAPI\Client\ApiException;
+use OpenAPI\Client\apisgen\DeploymentApi;
+use OpenAPI\Client\Model\Deployment;
+use OpenAPI\Client\Model\WorkersValue;
+use Upsun\UpsunClient;
+
 class WorkerTask extends TaskBase
 {
-    //fixme missing openapi endpoints $deployment->getWorkers()
+    public readonly DeploymentApi $api;
+
+    public function __construct(
+        public readonly UpsunClient $client,
+    )
+    {
+        $this->api = new DeploymentApi($this->client->apiClient, $this->client->apiConfig);
+    }
+
+    /**
+     * list workers of an environment
+     *
+     * @param string $projectId
+     * @param string $environmentId
+     * @return WorkersValue[]
+     * @throws ApiException
+     */
+    public function list(string $projectId, string $environmentId): array
+    {
+        $deployments = $this->api->listProjectsEnvironmentsDeployments($projectId, $environmentId);
+        $deployments = reset($deployments);
+        /** @var Deployment $deployments */
+        
+        return !empty($deployments) ? $deployments->getWorkers() : [];
+    }
 }
