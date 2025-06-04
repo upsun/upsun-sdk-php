a25af777ff7f7344fdcb0cbe130ae834386b6bf6
Florent Huck <flovntp@gmail.com>
2025-06-04 12:35:56 +0200

    debug get webapp

--- src/Core/Tasks/ApplicationTask.php
+++ src/Core/Tasks/ApplicationTask.php
@@ -4,6 +4,7 @@ namespace Upsun\Core\Tasks;
 
 use OpenAPI\Client\ApiException;
 use OpenAPI\Client\apisgen\DeploymentApi;
+use OpenAPI\Client\Model\Error;
 use OpenAPI\Client\Model\WebApplicationsValue;
 use Upsun\UpsunClient;
 
@@ -13,13 +14,14 @@ class ApplicationTask extends TaskBase
 
     public function __construct(
         public readonly UpsunClient $client,
-    ) {
+    )
+    {
         $this->api = new DeploymentApi($this->client->apiClient, $this->client->apiConfig);
     }
 
     /**
      * list applications of an environment
-     * 
+     *
      * @param string $projectId
      * @param string $environmentId
      * @return WebApplicationsValue[]
@@ -37,12 +39,16 @@ class ApplicationTask extends TaskBase
      *
      * @param string $projectId
      * @param string $environmentId
-     * @return WebApplicationsValue
+     * @return WebApplicationsValue|Error
      * @throws ApiException
      */
     public function get(string $projectId, string $environmentId, string $app_id): WebApplicationsValue
     {
-        $deployments = $this->api->listProjectsEnvironmentsDeployments($projectId, $environmentId);
+        $deployment = $this->api->listProjectsEnvironmentsDeployments($projectId, $environmentId);
+        $deployment = reset($deployment);
+
+        return $deployment[$app_id] ?? new Error(['message' => 'Application ' . $app_id . ' does not exist']);
+
         dd($deployments[0]->getWebapps());
         return $deployments[0]->getWebapps() ?? [];
     }
