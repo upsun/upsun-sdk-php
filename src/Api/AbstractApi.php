98796fdded03f091aefaa855ebf25e092c879314
Florent Huck <flovntp@gmail.com>
2025-08-28 15:17:36 +0200

    debug organizationApi

--- src/Api/AbstractApi.php
+++ src/Api/AbstractApi.php
@@ -55,7 +55,7 @@ abstract class AbstractApi
     ): ResponseInterface {
         $request = $this->createAuthenticatedRequest($method, $uri, $headers);
 
-
+        var_dump($request->getUri(), $request->getBody(), $request->getHeaders());
         try {
             return $this->httpClient->sendRequest($request);
         } catch (ClientExceptionInterface $e) {
